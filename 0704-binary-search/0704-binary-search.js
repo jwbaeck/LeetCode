24160d94236cca7ba3ed6e6dcbaf1a351d1c35d1
jwbaeck <jwbaeck97@gmail.com>
2024-04-03 10:58:09 +0900

    Time: 47 ms (92.82%), Space: 52.4 MB (30.09%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,18 @@
+function search(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    
+    if (target === nums[mid]) {
+      return mid;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else if (target < nums[mid]) {
+      right = mid - 1;
+    }
+  }
+  
+  return -1;
+}
