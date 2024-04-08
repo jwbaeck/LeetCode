b7cae7009e64313ee9517a0aa254b2917f0d954e
jwbaeck <jwbaeck97@gmail.com>
2024-04-08 21:42:31 +0900

    Time: 81 ms (38.65%), Space: 54.1 MB (56.78%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,15 @@
+function moveZeroes(nums) {
+    let noneZeroIndex = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== 0) {
+          nums[noneZeroIndex++] = nums[i];
+      }
+    }
+
+    for (let i = noneZeroIndex; i < nums.length; i++) {
+      nums[i] = 0;
+    }
+  
+  return nums;
+}
