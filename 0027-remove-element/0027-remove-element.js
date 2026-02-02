7eab73a557f6da3ea6f186c4dde789682b9790ca
jwbaeck <jwbaeck97@gmail.com>
2026-02-02 22:30:26 +0900

    Time: 0 ms (100%), Space: 54.1 MB (33.72%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+function removeElement(nums, val) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    if (nums[left] === val) {
+      nums[left] = nums[right];
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return left;
+}
