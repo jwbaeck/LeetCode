cca76e7767b8dd4a93ee90a75c56bd6c257981d6
jwbaeck <jwbaeck97@gmail.com>
2026-04-10 12:49:03 +0900

    Time: 0 ms (100%), Space: 54.4 MB (20.11%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums) {
+  let left = 0;
+  let current = 0;
+  let right = nums.length - 1;
+
+  while (current <= right) {
+    if (nums[current] === 0) {
+      [nums[left], nums[current]] = [nums[current], nums[left]];
+      left++;
+      current++;
+    } else if (nums[current] === 2) {
+      [nums[current], nums[right]] = [nums[right], nums[current]];
+      right--;
+    } else {
+      current++;
+    }
+  }
+
+  return nums;
+}
