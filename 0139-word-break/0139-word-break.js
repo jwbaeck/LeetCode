85ef4dd5d73f2caa00f4a8ba03529e8fc305d4ce
jwbaeck <jwbaeck97@gmail.com>
2026-06-10 01:54:55 +0900

    Time: 6 ms (64.52%), Space: 55.5 MB (76.89%) - LeetHub

---
+++ 0139-word-break/0139-word-break.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+function wordBreak(s, wordDict) {
+  const wordSet = new Set(wordDict);
+  const dp = Array(s.length + 1).fill(false);
+
+  dp[0] = true;
+
+  for (let end = 1; end <= s.length; end++) {
+    for (let start = 0; start < end; start++) {
+      const word = s.slice(start, end);
+
+      if (dp[start] && wordSet.has(word)) {
+        dp[end] = true;
+        break;
+      }
+    }
+  }
+
+  return dp[s.length];
+}
