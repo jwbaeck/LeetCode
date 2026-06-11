80b20f58e48ecc88c86112c90c91353aa8bc7bf0
jwbaeck <jwbaeck97@gmail.com>
2026-06-11 19:48:41 +0900

    Time: 0 ms (100%), Space: 60.4 MB (35.63%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function isBalanced(root) {
+  function checkHeight(node) {
+    if (!node) return 0;
+
+    const leftHeight = checkHeight(node.left);
+
+    if (leftHeight === -1) return -1;
+
+    const rightHeight = checkHeight(node.right);
+    
+    if (rightHeight === -1) return -1;
+
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  return checkHeight(root) !== -1;
+}
