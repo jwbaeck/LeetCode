a7adda517c8575e1d6543b7ee63271329d638e7e
jwbaeck <jwbaeck97@gmail.com>
2026-06-16 22:11:51 +0900

    Time: 49 ms (55.46%), Space: 57.6 MB (50.48%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function detectCycle(head) {
+  if (head === null) return null;
+
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      let start = head;
+
+      while (start !== slow) {
+        start = start.next;
+        slow = slow.next;
+      }
+
+      return start;
+    }
+  }
+
+  return null;
+}
