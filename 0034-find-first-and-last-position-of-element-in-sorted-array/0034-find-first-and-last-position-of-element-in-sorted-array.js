dcce784b129bdfcdfc910dbc5c98d46c626188df
jwbaeck <jwbaeck97@gmail.com>
2026-08-11 06:36:52 +0900

    Time: 0 ms (100%), Space: 54.4 MB (70.77%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function searchRange(nums, target) {
+  function findFirst() {
+    let left = 0;
+    let right = nums.length - 1;
+    let result = -1;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (nums[mid] >= target) {
+        if (nums[mid] === target) result = mid;
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return result;
+  }
+
+  function findLast() {
+    let left = 0;
+    let right = nums.length - 1;
+    let result = -1;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (nums[mid] <= target) {
+        if (nums[mid] === target) result = mid;
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return result;
+  }
+
+  return [findFirst(), findLast()];
+}
