6f730a86d05b5e80ed157057e1e86b2612fa6a16
jwbaeck <jwbaeck97@gmail.com>
2026-08-20 08:59:35 +0900

    Time: 2 ms (68.42%), Space: 57.9 MB (94.2%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function canJump(nums) {
+  let maxReach = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxReach) {
+      return false;
+    }
+
+    maxReach = Math.max(maxReach, i + nums[i]);
+
+    if (maxReach >= nums.length - 1) {
+      return true;
+    }
+  }
+
+  return true;
+}
