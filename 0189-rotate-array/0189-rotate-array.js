80570bc394a30ac00fc307331a6ee0ad2e391d09
jwbaeck <jwbaeck97@gmail.com>
2026-08-20 09:29:37 +0900

    Time: 9 ms (13.26%), Space: 97.4 MB (5.24%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums, k) {
+  k = k % nums.length;
+
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+}
+
+function reverse(nums, left, right) {
+  while (left < right) {
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+    
+    left++;
+    right--;
+  }
+}
