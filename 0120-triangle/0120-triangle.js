5255ccfba50ad332e2d12e8af2d58c60acbb798f
jwbaeck <jwbaeck97@gmail.com>
2026-09-01 13:38:51 +0900

    Time: 5 ms (25.26%), Space: 53.3 MB (95.79%) - LeetHub

---
+++ 0120-triangle/0120-triangle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+function minimumTotal(triangle) {
+  const dp = [...triangle[triangle.length - 1]];
+
+  for (let row = triangle.length - 2; row >= 0; row--) {
+    for (let col = 0; col < triangle[row].length; col++) {
+      dp[col] =
+        triangle[row][col] +
+        Math.min(dp[col], dp[col + 1]);
+    }
+  }
+
+  return dp[0];
+}
