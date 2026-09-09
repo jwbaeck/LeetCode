fb3c75a2ce547ab97848e3959afc51f6dbd443a4
jwbaeck <jwbaeck97@gmail.com>
2026-09-09 23:24:37 +0900

    Time: 40 ms (49.09%), Space: 80.6 MB (52.55%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function longestConsecutive(nums) {
+  const numSet = new Set(nums);
+  let maxLength = 0;
+
+  for (const num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentLength = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentLength++;
+      }
+
+      maxLength = Math.max(maxLength, currentLength);
+    }
+  }
+
+  return maxLength;
+}
